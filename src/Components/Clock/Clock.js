ade8cdcabf37388c05572bfb6ffe38e5d3f35a5c
Berkay Çelik <berkayc96@hotmail.com>
2018-09-02 00:38:08 +0300

    try

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -26,7 +26,7 @@ class Clock extends React.Component {
 
     render() {
         return (
-            <div className="clock">
+            <div className="clock newClass">
                   <h2> Saat : {this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
